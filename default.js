33110cfbc6143ee3fe39b474bbfc4adc16af38df
lightning-shark <109218966+lightning-shark@users.noreply.github.com>
2022-07-29 12:22:12 +0800

    Add files via upload
    Main

---
+++ default.js
@@ -0,0 +1,6 @@
+let currentpoint = 0;
+let remaining = 100;
+function points() {
+    var z = document.getElementsByClassName("points");
+    alert("You have " + currentpoint + " points.\n" + remaining + " more points to go to next level.");
+}
